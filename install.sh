700417a46376fd9923976f0887bf559487107d68
erik bergsten <erikbergsten94@gmail.com>
2022-12-04 19:22:34 +0000

    cool stuff

--- install.sh
+++ install.sh
@@ -8,4 +8,7 @@ ln -sf $PWD/.vim ~/.vim
 rm ~/.tmux.conf
 ln -sf $PWD/.tmux.conf ~/.tmux.conf
 
+mkdir -p ~/.config/nvim
+ln -sf  ~/.vimrc ~/.config/nvim/init.vim
+
 echo "source $PWD/cool.rc" >> ~/.bashrc
