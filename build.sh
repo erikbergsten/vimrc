8f29d228e6f93a06e61d13a2c15c41802efa3091
erik bergsten <erikbergsten94@gmail.com>
2023-12-03 17:58:38 +0100

    fix build script docker usage

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
-BUN=bun
-HOME=$(pwd)
 
-if command -v docker $> /dev/null
-then
-  BUN="docker run -it --rm --name bun -v `pwd`:/vimrc --workdir=/vimrc oven/bun"
-  HOME=/vimrc
-fi
 
 build_metrics() {
+  BUN=bun
+  HOME=$(pwd)
+  if command -v docker $> /dev/null
+  then
+    BUN="docker run -it --rm --name bun -v `pwd`:/vimrc --workdir=/vimrc/tools/git-metrics oven/bun"
+    HOME=/vimrc
+  fi
   echo "Building git-metrics!"
   set -x
   cd tools/git-metrics
-  $BUN install
+  $BUN -- install
   $BUN build ./git-metrics.ts --compile --outfile $HOME/bin/git-metrics
   set +x
   cd ../..
