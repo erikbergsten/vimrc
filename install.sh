ce89e9d7ccbd2eb79f77efffb08367876ee4638c
erik bergsten <erikbergsten94@gmail.com>
2025-12-14 22:56:34 +0100

    a nice full layout

--- install.sh
+++ install.sh
@@ -10,9 +10,9 @@ ln -sf $PWD/.tmux.conf ~/.tmux.conf
 
 ln -sf  $PWD/nvim ~/.config/
 
-echo "source $PWD/cool.rc" >> ~/.bashrc
-echo "export PATH=\$PATH:$PWD/bin" >> ~/.bashrc
 echo "export VIMPATH=$PWD" >> ~/.bashrc
+echo "export PATH=\$PATH:$PWD/bin" >> ~/.bashrc
+echo "source $PWD/cool.rc" >> ~/.bashrc
 
 chmod +x bin/kubedev.sh
 chmod +x bin/remote_sync.sh
